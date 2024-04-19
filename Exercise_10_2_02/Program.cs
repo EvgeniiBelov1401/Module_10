2e840513c4aef337b25c77dc70dfe8390517cb7d
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-19 10:34:10 +0300

    Exercise_10_2_02 - явная реализация

---
+++ Exercise_10_2_02/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise_10_2_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Writer writer = new Writer();
+            ((IWriter)writer).Write();
+        }
+    }
+}
---
+++ Exercise_10_2_02/Writer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10_2_02
+{
+    internal class Writer : IWriter
+    {
+        void IWriter.Write()
+        {
+            
+        }
+    }
+}
