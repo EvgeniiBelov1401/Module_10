0c564ecda5dd2410fa0b0d73b2b59f3165cbdf6d
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-19 10:28:40 +0300

    Exercise_10_2_003 - неявная реализация

---
+++ Exercise_10_2_003/IViber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10_2_003
+{
+    internal interface IViber
+    {
+        public void SendMessage(string message, int count);
+    }
+}
---
+++ Exercise_10_2_003/Messanger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10_2_003
+{
+    internal class Messanger : IWhatsApp, IViber
+    {
+        public void SendMessage(string message)
+        {
+            
+        }
+
+        public void SendMessage(string message, int count)
+        {
+            
+        }
+    }
+}
