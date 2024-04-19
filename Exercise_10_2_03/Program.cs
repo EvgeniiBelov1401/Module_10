a0a482fe0a6d1c29d5e83752137029977d668457
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-19 10:36:43 +0300

    Exercise_10_2_03 - неявная реализация

---
+++ Exercise_10_2_03/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise_10_2_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            worker.Build();
+        }
+    }
+}
---
+++ Exercise_10_2_03/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10_2_03
+{
+    internal class Worker : IWorker
+    {
+        public void Build()
+        {
+            
+        }
+    }
+}
