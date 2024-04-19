3785ce89b4848725eba32129f0e08bf12eaff2c3
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-19 10:41:34 +0300

    Exercise_10_2_04 - явная реализация

---
+++ Exercise_10_2_04/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise_10_2_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            ((IWorker)worker).Build();
+        }
+    }
+}
