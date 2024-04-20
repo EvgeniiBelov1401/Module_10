8492202be4b07f1e703058fd74f324ab5e80fb67
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-20 10:39:42 +0300

    Exercise_10_3_01 - множественная реализация интерфейсов

---
+++ Exercise_10_3_01/FileManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10_3_01
+{
+    internal class FileManager : IWriter, IReader, IMailer
+    {
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Exercise_10_3_01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise_10_3_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
