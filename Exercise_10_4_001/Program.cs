c1b57d1de3f1b85773c27b823c3e080305458c84
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-21 13:39:07 +0300

    Exercise_10_4_001 - обобщенный интерфейс

---
+++ Exercise_10_4_001/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise_10_4_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Exercise_10_4_001/Viber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10_4_001
+{
+    internal class Viber : IMessanger<Phone>, IMessanger<Computer>
+    {
+        Phone IMessanger<Phone>.DeviceInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        Computer IMessanger<Computer>.DeviceInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
