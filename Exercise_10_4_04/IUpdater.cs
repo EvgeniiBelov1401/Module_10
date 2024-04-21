00833991f31b963e5a8140a12693a22203bfeea5
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-21 14:50:41 +0300

    Exercise_10_4_04 - обобщенный интефейс - контрвариация

---
+++ Exercise_10_4_04/IUpdater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10_4_04
+{
+    internal interface IUpdater<in T>
+    {
+        void Update(T entity);
+    }
+}
---
+++ Exercise_10_4_04/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise_10_4_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IUpdater<User> updater1 = new UserService();
+            IUpdater<Accounr>updater2= new UserService();
+        }
+    }
+}
